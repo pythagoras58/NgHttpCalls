4bef101ae8eb1ab2d2f9cf4866bbf5348bd0e071
pythagoras58 <solomondanso58@gmail.com>
2023-01-05 23:50:57 +0000

    user details design 1

--- src/app/component/userdetails/userdetails.component.ts
+++ src/app/component/userdetails/userdetails.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap} from "@angular/router";
 import {UserService} from "../../service/user.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-userdetails',
   templateUrl: './userdetails.component.html',
   styleUrls: ['./userdetails.component.css']
 })
-export class UserdetailsComponent implements OnInit {
+export class UserdetailsComponent implements OnInit, OnDestroy {
+
+  subscribedUserDetail : Subscription;
 
   constructor(private activateRoute: ActivatedRoute, private userService:UserService) { }
 
   ngOnInit(): void {
+    this.subscribedUserDetail = this.activateRoute.paramMap.subscribe((params : ParamMap)=>{
+      this.userService.getUser(+params.get('uuid')!).subscribe(
+        (response : any)=>{
+          console.log(response);
+        }
+      )
+    })
+  }
+
+  ngOnDestroy(): void {
   }
 
 }
