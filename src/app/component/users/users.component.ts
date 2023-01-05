19a1a82a1b5ed8b42f87afad0f96bf47bee6618b
pythagoras58 <solomondanso58@gmail.com>
2023-01-05 15:58:44 +0000

    DI user service into user 1

--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from "../../service/user.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  usersSubscription : Subscription | undefined;
+  userSubscription : Subscription | undefined;
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
+    this.usersSubscription =  this.userService.getUsers(15).subscribe(
+      (res : any)=>{
+        console.log(res)
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.usersSubscription?.unsubscribe();
   }
 
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,14 @@ export class UserService {
 
   private readonly  apiUrl:string = 'https://randomuser.me/api';
 
-  constructor(private http:HttpClient, private userService:UserService) { }
+  constructor(private http:HttpClient) { }
 
   // fETCH USERS
-
+  getUsers(size : number):Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/?results=${size}`);
+  }
   // FETCH USER BY UUID
+  getUser(uuid : number = 1):Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/?uuid=${uuid}`);
+  }
 }
