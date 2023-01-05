cdd240409a4cbc27b0604c29da269403a4c7a170
pythagoras58 <solomondanso58@gmail.com>
2023-01-05 19:54:17 +0000

    User Interface

---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,16 @@
+
+export interface UserInterface {
+    uuid:string;
+    firstName:string;
+    lastName:string;
+    email:string;
+    userName:string;
+    gender:string;
+    address:string;
+    dateOfBirth:string;
+    phone:string;
+    imageUrl:string;
+    coordinate: Coordinate;
+}
+
+
