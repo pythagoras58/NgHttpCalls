9e020d97d6beb2a06589d3f370ae4e309511dadb
pythagoras58 <solomondanso58@gmail.com>
2023-01-06 15:05:48 +0000

    Add resolver done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UsersComponent} from "./component/users/users.component";
 import {UserdetailsComponent} from "./component/userdetails/userdetails.component";
+import {UserResolver} from "./service/user.resolver";
 
 const routes: Routes = [
   {path: 'users', component:UsersComponent},
-  {path: 'user/:uuid', component:UserdetailsComponent},
+  {path: 'user/:uuid', component:UserdetailsComponent, resolve: {resolvedResponse : UserResolver}},
   {path: '**', redirectTo: 'users'}
 ];
 
--- src/app/service/user.resolver.ts
+++ src/app/service/user.resolver.ts
@@ -5,12 +5,17 @@ import {
   ActivatedRouteSnapshot
 } from '@angular/router';
 import { Observable, of } from 'rxjs';
+import {ResponseInterface} from "../interfaces/Response.interface";
+import {UserService} from "./user.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserResolver implements Resolve<boolean> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return of(true);
+export class UserResolver implements Resolve<ResponseInterface> {
+
+  constructor(private userService:UserService) {
+  }
+  resolve(route: ActivatedRouteSnapshot, _: RouterStateSnapshot): Observable<ResponseInterface> {
+    return this.userService.getUser(route.paramMap.get('uuid')!);
   }
 }
